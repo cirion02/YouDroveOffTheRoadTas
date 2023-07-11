27829eb921a0568fe63806789aeeb122e89d5055
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-07-11 14:47:39 +0200

    Added seeing red

---
+++ tasFiles/seeing_red.js
@@ -0,0 +1,25 @@
+tasFiles.push({
+loadLevel : "seeing red",
+//skipFrames: 1500,
+//frameAdvance : 10,
+inputs : `
+320:800,500
+120:800,400
+135:800,0
+715:0,390
+200:800,800
+192:800,450
+`,
+
+//parts after this aren't used by the code
+time:"0:01.569",
+
+backup: `
+320:800,500
+120:800,400
+135:800,0
+715:0,390
+200:800,800
+192:800,450
+`
+})
